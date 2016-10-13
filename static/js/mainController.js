cf3e1c0383241fe6a910ac0d3b28fcfc969b0bf2
jttwnsnd <jt.a.townsend@gmail.com>
2016-10-13 15:53:20 -0400

    Add Register and Login Functions

--- static/js/mainController.js
+++ static/js/mainController.js
@@ -2,6 +2,41 @@ var janusApp = angular.module('janusApp', ['ngRoute', 'ngCookies'])
 janusApp.controller('mainController', function($scope, $http, $location, $cookies, $timeout){
 
 	$scope.test = "test!"
+//===================
+// -- REGISTER --
+//===================
+	$scope.register = function(){
+		$http.post(path + 'register_submit', {
+			username: $scope.username,
+			password: $scope.password,
+			avatar: $scope.avatar
+		}).then(function success(response){
+			if(response.data == 'reg successful'){
+				$scope.login();
+				$('.dropdown.open .dropdown-toggle').dropdown('toggle');
+			}
+		})
+
+	}
+//===================
+// -- LOGIN --
+//===================
+	$scope.login = function(){
+		$http.post(path + 'login_submit', {
+			username: $scope.username,
+			password: $scope.password
+		}).then(function successCallback(response){
+			if (response.data){
+				$scope.loggedIn = true;
+				$scope.signedInAs = $scope.username;
+				$cookies.put('username', $scope.username);
+				$scope.avatar = response.data;
+				$cookies.put('avatar', $scope.avatar)
+			}
+			getTrendingUsers();
+			loadPosts();
+		})
+	}
 
 })
 
