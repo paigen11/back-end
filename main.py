1c77263b0226c8a7a8a2e224ee2492fab4d8a125
paigen11 <paigen11@gmail.com>
2016-10-13 11:39:29 -0400

    First commit

---
+++ main.py
@@ -0,0 +1,23 @@
+# import flask stuff
+from flask import Flask, render_template, redirect, request, jsonify, session
+# import the mysql module
+from flaskext.mysql import MySQL
+import bcrypt as bcrypt
+
+# set up mysql connection here later
+mysql = MySQL()
+app = Flask(__name__)
+app.config['MYSQL_DATABASE_USER'] = 'x'
+app.config['MYSQL_DATABASE_PASSWORD'] = 'x'
+app.config['MYSQL_DATABASE_DB'] = 'janus'
+app.config['MYSQL_DATABASE_HOST'] = '127.0.0.1'
+mysql.init_app(app)
+
+conn = mysql.connect()
+cursor = conn.cursor()
+
+app.secret_key = "FHNOGVOIWHWNQFQW(FHGNRUOGEWOUGHEW"
+
+@app.route('/')
+def index():
+	return render_template('index.html')
