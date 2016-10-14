3fb6ebe936b43b848c1844993423f474e8331647
ericettensohn <ericettensohn@gmail.com>
2016-10-14 12:36:31 -0400

    Added Masonry to controller

--- static/js/mainController.js
+++ static/js/mainController.js
@@ -4,6 +4,19 @@ janusApp.controller('mainController', function($scope, $http, $location, $cookie
 // -- VARIABLES --
 //===================
 	var path = 'http://localhost:5000/';
+
+//===================
+// -- MASONRY --
+//===================
+
+	$('.grid').masonry({
+			// set itemSelector so .grid-sizer is not used in layout
+		itemSelector: '.grid-item',
+		// use element for option
+		// columnWidth: 200,
+		percentPosition: true
+	});
+
 //===================
 // -- REGISTER --
 //===================
